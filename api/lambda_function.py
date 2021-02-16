401e81e6580c76c19266f743390a7d575ea8f387
Gregory Mundy <gregmundy@gmail.com>
2021-02-15 22:03:07 -0500

    feat: add some sample data to api

--- api/lambda_function.py
+++ api/lambda_function.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 from mangum import Mangum
 from v1.routers import router
 
-app = FastAPI(title='Project Signal Data Resources API', description='A collection of APIs for Project Signal', openapi_prefix='/dev')
+app = FastAPI(title='Project Signal Data Resources API',
+              description='A collection of APIs for Project Signal',
+              openapi_prefix='/dev')
 
 app.include_router(router)
 
+
 @app.get('/')
 def get_health():
-    return { 'message': 'OK'}
+    return {'message': 'OK'}
+
 
 lambda_handler = Mangum(app=app)
-
--- api/v1/endpoints/programs.py
+++ api/v1/endpoints/programs.py
@@ -1,7 +1,29 @@
 from fastapi import APIRouter
+from typing import Optional
+from pydantic import BaseModel
 
 router = APIRouter()
 
-@router.get("/programs/{organization_id}")
-async def get_programs(organization_id: str):
-    return { 'endpoint': 'programs' }
+
+programs = {
+    'abc123': {'programId': 'abc123', 'programName': 'Program 1 Description',
+               'programDescription': 'Program 1'},
+    'abc456': {'programId': 'abc456', 'programName': 'Program 2 Description',
+               'programDescription': 'Program 2 Description'},
+    'abc789': {'programId': 'abc789', 'programName': 'Program 3',
+               'programDescription': 'Program 3 Description'},
+}
+
+
+class Program(BaseModel):
+    programId: str
+    programName: str
+    programDescription: Optional[str] = None
+
+
+@router.get("/programs/{organization_id}", response_model=Program)
+async def get_programs(organization_id: str, program_id: Optional[str] = None):
+    table_name = f'programs_{organization_id}'
+    if program_id:
+        return programs[program_id]
+    return {'table name': f'{table_name}'}
